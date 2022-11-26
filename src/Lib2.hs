1c1e3c4ca94acbfd71ad93dcc6eaa30bce04e7f0
Danielius <100567065+Danielius4@users.noreply.github.com>
2022-11-26 19:57:54 +0200

    DocToYAML parse function

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1,7 +1,225 @@
+-- module Lib2 where
+
+-- import  Control.Applicative
+-- import Data.Char
+
+-- {-
+-- IMPLEMENTED:
+-- DNull, DInteger(positive, negative), DString(with quotes, without quotes)
+
+-- NOT IMPLEMENTED:
+-- DList, DMap
+-- -}
+
+
+-- data Document =
+--     DMap [(String, Document)]
+--     | DList [Document]
+--     | DInteger Int
+--     | DString String
+--     | DNull
+--     deriving (Show, Eq)
+
+-- newtype Parser a = Parser 
+--   { runParser :: String -> Either String (a, String) 
+--   }
+
+-- instance Functor Parser where
+--   fmap f (Parser p) = Parser $ \input -> do
+--     (x, rest) <- p input
+--     return (f x, rest)
+
+-- instance Applicative Parser where
+--   pure x = Parser $ \input -> Right (x, input)
+--   (Parser p1) <*> (Parser p2) = Parser $ \input -> do
+--     (f, rest) <- p1 input
+--     (a, rest') <- p2 rest
+--     Right $ (f a, rest')
+
+-- instance Alternative Parser where
+--   empty = Parser $ const $ Left "empty"
+--   (Parser p1) <|> (Parser p2) = Parser $ \input ->
+--     case p1 input of
+--       Left _ -> p2 input
+--       x      -> x
+
+-- parseChar :: Char -> Parser Char
+-- parseChar c = Parser $ \input -> 
+--   case input of
+--     (x : xs) | x == c -> Right (c, xs)
+--              | otherwise -> Left $ "Expected '" ++ [c] ++ "'," ++ " but got '" ++ [x] ++ "'"
+--     [] -> Left $ "Expected '" ++ [c] ++ "', but got empty string"
+
+-- parseStr :: String -> Parser String
+-- parseStr str = Parser $ \input ->
+--   let result = runParser (traverse parseChar str) input in
+--   case result of
+--     Left _ -> Left $ "Expected \"" ++ str ++ "\", but got \"" ++ input ++ "\""
+--     _ -> result
+
+-- spanParserMany :: (Char -> Bool) -> Parser String
+-- spanParserMany f = many $ parseIf f
+
+-- spanParserSome :: (Char -> Bool) -> Parser String
+-- spanParserSome f = some $ parseIf f
+
+-- parseIf :: (Char -> Bool) -> Parser Char
+-- parseIf f =
+--   Parser $ \input ->
+--     case input of
+--       y:ys
+--         | f y       -> Right (y, ys)
+--         | otherwise -> Left $ "Expected a character, but got '" ++ [y] ++ "'"
+--       [] -> Left $ "Expected a character, but reached end of string"
+
+
+-- intLiteral :: Parser Int
+-- intLiteral = (parseChar '-' *> (fmap negate nonNegativeLiteral)) <|> nonNegativeLiteral
+
+-- nonNegativeLiteral :: Parser Int
+-- nonNegativeLiteral = read <$> spanParserSome isDigit
+
+-- stringLiteral :: Parser String
+-- stringLiteral = spanParserMany isAlphaNum 
+
+-- parseDNull :: Parser Document
+-- parseDNull = Parser $ \input -> do
+--   let result = runParser (parseStr "null") input in
+--     case result of
+--       Left a -> Left a
+--       _ -> Right $ (DNull, drop 4 input)
+
+-- parseDInteger :: Parser Document
+-- parseDInteger = DInteger <$> intLiteral
+
+-- parseDString :: Parser Document
+-- parseDString = DString <$> ( optional (parseChar '"') *> stringLiteral <* parseIf (\char -> char /= ':') <* optional (parseChar '"'))
+
+-- --Parse a YAML list
+
+-- {-
+
+-- parseDList :: Parser Document
+-- parseDList = Parser $ \input -> do
+--   l <- lines input
+--   case l of
+--     x:xs
+--       | (,) <$>  <*> (parseStr "- " *> unlines l)
+--       | otherwise -> Left $ "Expected a list, but got '" ++ x ++ "'"
+--     [] -> Left $ "Expected a list, but got empty string"
+-- -}
+-- {-
+
+--   let result = runParser (parseStr "- ") x in
+--     case result of
+--       Left a -> Left a
+--       _ -> Right $ (DList, drop 2 x)
+
+-- -}
+
+-- --Parse a YAML map
+
+-- parseDMap :: Int -> Parser Document
+-- parseDMap depth = do
+--   DMap <$> many (parsePair depth)
+
+-- -- parsePair :: Int -> Parser (String, Document)
+-- -- parsePair depth = Parser $ \input ->
+-- --   do
+-- --     result <- runParser countSpaces input
+-- --     case result of
+-- --       Right (num, rest)
+-- --         let indentation = (num / 2) in
+-- --         case indentation of
+-- --           Right (indentation, rest) ->
+-- --             if depth == indentation 
+-- --               then parseDocumentWID 
+-- --             else if depth > indentation 
+-- --               then Left "error"
+-- --             else parseDocumentWID
+-- --       Left a -> Left a
+-- --     Left _ -> Left $ "Expected a pair, but got '" ++ input ++ "'"
+
+-- -- parsePair :: Int -> Parser (String, Document)
+-- -- parsePair depth = Parser $ \input ->
+-- --   let result = runParser countSpaces input in
+-- --     case result of
+-- --       Right (num, rest) ->
+-- --         if depth == (num / 2) then (,) <$> stringLiteral <*> (parseStr ":" *> spanParserSome isSpace *> 
+-- --                                                               parseDocumentWID depth 
+-- --                                                               <* (optional (parseChar '\n')))
+-- --         else if depth > (num / 2) then Left "error"
+-- --         else (,) <$> stringLiteral <*> (parseStr ":" *> spanParserSome isSpace *> 
+-- --                                         parseDocument depth 
+-- --                                         <* (optional (parseChar '\n')))
+-- --       Left a -> Left a
+
+-- parsePair :: Int -> Parser (String, Document)
+-- parsePair depth =
+--   if depth == spaces then (,) <$> stringLiteral <*> (parseStr ":" *> spanParserSome isSpace *> 
+--                                                     parseDocumentWID depth 
+--                                                     <* (optional (parseChar '\n')))
+--   else 
+--   where spaces = 2
+
+
+
+-- -- parsePair :: Int -> Parser (String, Document)
+-- -- parsePair depth = (,) <$> stringLiteral <*> (parseStr ":" *> spanParserSome isSpace *> parseDocument depth <* (optional (parseChar '\n')))
+
+
+-- countChars :: Parser Int
+-- countChars = length <$> spanParserMany isDashOrSpace
+
+-- countSpaces :: Parser Int
+-- countSpaces = length <$> spanParserMany isSpace
+
+-- isDashOrSpace :: Char -> Bool
+-- isDashOrSpace c = c == '-' || isSpace c
+
+
+-- parseDocument :: Int -> Parser Document
+-- parseDocument depth = parseDInteger <|> parseDNull <|> parseDString <|> (parseDMap (depth+1))  -- <|> parseDList  <|> parseDMap
+
+-- parseDocumentWID :: Int -> Parser Document
+-- parseDocumentWID depth = parseDInteger <|> parseDNull <|> parseDString <|> (parseDMap depth)
+
+-- parseDIntegerInDMap :: Int -> Parser Document
+-- parseDIntegerInDMap depth = undefined
+
+-- parseDNullInDMap :: Int -> Parser Document
+-- parseDNullInDMap depth = undefined
+
+-- parseDStringInDMap :: Int -> Parser Document
+-- parseDStringInDMap = undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module Lib2(renderDocument, hint, gameStart) where
 
 import Types ( Document(..))
 import Lib1 (State(..))
+import  Control.Applicative
+import Data.Char
 
 -- !
 -- How to enter GHCI:
@@ -193,56 +411,281 @@ parseDoc' (DMap((str, val) : xs)) acc =
 parseDoc' (DMap[]) acc = acc ++ "}"
 
 
+
+
+
+
 -- fuctions to parse in NOT FLOW type YAML  -- 
+
+-- DMap for testing purposes
+-- DMap[("coords", DList[DMap[("col", DInteger 0),("row", DInteger 2)], DMap[("col", DInteger 0), ("row", DInteger 5)], DMap[("col", DInteger 2), ("row", DInteger 3)], DMap[("col", DInteger 3), ("row", DInteger 3)], DMap[("col", DInteger 4), ("row", DInteger 5)], DMap[("col", DInteger 2), ("row", DInteger 7)], DMap[("col", DInteger 3), ("row", DInteger 7)], DMap[("col", DInteger 4), ("row", DInteger 7)], DMap[("col", DInteger 6), ("row", DInteger 2)], DMap[("col", DInteger 6), ("row", DInteger 3)], DMap[("col", DInteger 6), ("row", DInteger 5)], DMap[("col", DInteger 6), ("row", DInteger 6)], DMap[("col", DInteger 6), ("row", DInteger 7)], DMap[("col", DInteger 6), ("row", DInteger 8)], DMap[("col", DInteger 8), ("row", DInteger 0)], DMap[("col", DInteger 8), ("row", DInteger 1)], DMap[("col", DInteger 8), ("row", DInteger 8)], DMap[("col", DInteger 9), ("row", DInteger 4)], DMap[("col", DInteger 9), ("row", DInteger 5)], DMap[("col", DInteger 9), ("row", DInteger 6)]])]
 
 
 parseDocToYaml :: Document -> String
 
-parseDocToYaml doc = docToString doc "---" ""
+parseDocToYaml doc = docToString doc "---\n" "" 0
 
 
-docToString :: Document -> String -> String -> String
+docToString :: Document -> String -> String -> Int -> String
 
-docToString DNull acc spaces = acc ++ "null"
+docToString DNull acc spaces nest = acc ++ "null"
 
-docToString (DInteger d) acc spaces = acc ++ show d
+docToString (DInteger x) acc spaces nest = acc ++ (show x)
 
-docToString (DString d) acc spaces = acc ++ d
+docToString (DString x) acc spaces nest = acc ++ x
 
-docToString (DList (x : xs)) acc spaces =
-  docToString' (DList xs) (docToString x (acc ++ "\n" ++ spaces ++ "- ") (spaces ++ "  ")) (spaces ++ "  ")
+docToString (DList(DMap x: xs)) acc spaces nest =
+  docToString' (DList xs) (docToString (DMap x) (acc ++ (printSpaces nest acc) ++ "- ") (spaces ++ "  ") (nest + 1)) (spaces ++ "") nest
 
-docToString (DList []) acc spaces =
-  acc ++ "[]"
+docToString (DList(DList x: xs)) acc spaces nest =
+  docToString' (DList xs) (docToString (DList x) (acc ++ (printSpaces nest acc) ++ "- ") (spaces ++ "  ") (nest + 1)) (spaces ++ "") nest
 
-docToString (DMap((str, DList val) : xs)) acc spaces =
-  docToString' (DMap xs) (docToString (DList val) (acc ++ "\n" ++ str ++ ":") spaces) spaces
+docToString(DList (x : xs)) acc spaces nest =
+  docToString' (DList xs) (docToString x (acc ++ (printSpaces nest acc)++ "- ") spaces nest) spaces nest
 
-docToString (DMap((str, val) : xs)) acc spaces =
-  docToString' (DMap xs) (docToString val (acc ++ str ++ ": ") spaces) (spaces ++ "")
+docToString(DList[]) acc spaces nest = acc ++ "[]"
 
-docToString (DMap[]) acc spaces = acc ++ "{}"
+docToString (DMap ((str, DMap []) : xs)) acc spaces nest =
+  docToString' (DMap xs) (docToString (DMap []) (acc ++ (printSpaces nest acc) ++ str ++ ": ") spaces (nest + 1)) spaces nest
 
---inner functions for parsing----
+docToString (DMap ((str, DMap doc) : xs)) acc spaces nest =
+  docToString' (DMap xs) (docToString (DMap doc) (acc ++ (printSpaces nest acc) ++ str ++ ":\n") spaces (nest + 1)) spaces nest
 
-docToString' :: Document -> String -> String -> String
+docToString (DMap ((str, DList doc) : xs)) acc spaces nest =
+  docToString' (DMap xs) (docToString (DList doc) (acc ++ (printSpaces nest acc) ++ str ++ ":\n") spaces (nest)) spaces nest
 
-docToString' DNull acc spaces = acc ++ "null"
+docToString (DMap ((str, doc) : xs)) acc spaces nest =
+  docToString' (DMap xs) (docToString doc (acc ++ (printSpaces nest acc) ++ str ++ ": ") spaces nest) spaces nest
 
-docToString' (DInteger d) acc spaces = acc ++ show d
+docToString (DMap[]) acc paces nest = acc ++ "{}"
 
-docToString' (DString d) acc spaces = acc ++ d
 
-docToString' (DList (x : xs)) acc spaces =
-  docToString' (DList xs) (docToString x (acc ++ "\n" ++ init(init(spaces)) ++ "- ") spaces) spaces
+docToString' :: Document -> String -> String -> Int -> String
 
-docToString' (DList []) acc spaces=
-  acc ++ ""
+docToString' DNull acc spaces nest = acc ++ "null"
 
-docToString' (DMap((str, val) : xs)) acc spaces=
-  docToString' (DMap xs) (docToString val (acc ++ "\n" ++ spaces ++ str ++ ": ") spaces) spaces
+docToString' (DInteger x) acc spaces nest = acc ++ (show x)
 
-docToString' (DMap[]) acc spaces= acc ++ ""
+docToString' (DString x) acc spaces nest = acc ++ x
+
+docToString' (DList (DMap x : xs)) acc spaces nest =
+  docToString (DList(DMap x: xs)) (acc ++ "\n") spaces nest
+
+docToString' (DList (DList x : xs)) acc spaces nest =
+  docToString (DList(DList x: xs)) (acc ++ "\n") spaces nest
+
+
+docToString' (DList (x : xs)) acc spaces nest=
+  docToString' (DList xs) (docToString x (acc ++ "\n" ++ (printSpaces nest acc) ++ "- ") spaces nest) spaces nest
+
+docToString' (DList []) acc spaces nest = acc
+
+docToString' (DMap ((str, DMap []) : xs)) acc spaces nest =
+  docToString' (DMap xs) (docToString (DMap []) (acc ++ (printSpaces nest acc) ++ str ++ ": ") spaces (nest + 1)) spaces nest
+
+docToString' (DMap ((str, DMap doc) : xs)) acc spaces nest =
+  docToString' (DMap xs) (docToString (DMap doc) (acc ++ "\n"  ++ (printSpaces nest acc) ++ str ++ ":\n") spaces (nest + 1)) spaces nest
+
+docToString' (DMap ((str, DList doc) : xs)) acc spaces nest =
+  docToString' (DMap xs) (docToString (DList doc) (acc ++ "\n"  ++ (printSpaces nest acc) ++ str ++ "- ") spaces (nest)) spaces nest
+
+docToString' (DMap ((str, doc) : xs)) acc spaces nest =
+  docToString' (DMap xs) (docToString doc (acc ++ "\n" ++ (printSpaces nest acc) ++ str ++ ": ") spaces nest) spaces nest
+
+docToString' (DMap[]) acc paces nest = acc
+
+
+
+printSpaces :: Int -> String -> String
+
+printSpaces 0 str = ""
+
+printSpaces count [] = 
+  "  " ++ (printSpaces (count - 1) [])
+
+printSpaces count str = 
+  if (take 2 (reverse str)) /= " -"
+    then"  " ++ (printSpaces (count - 1) str)
+  else ""
+
+{-
+
+docToString'' :: Document -> String -> String -> String    -- for parsing the first elem of List in other List
+
+docToString'' DNull acc spaces = acc ++ "null"
+
+docToString'' (DInteger x) acc spaces = acc ++ (show x)
+
+docToString'' (DString x) acc spaces = acc ++ x
+
+
+docToString'' (DList (DList x : xs)) acc spaces =
+  docToString' (DList xs) (docToString'' (DList x) (acc ++ "- ") (spaces ++ "  ")) spaces
+
+docToString'' (DList (x : xs)) acc spaces =
+  docToString' (DList xs) (docToString x (acc ++ "- ") (spaces ++ "")) (spaces ++ "  ")
+
+docToString'' (DList []) acc spaces = acc
+
+                                                        -- DMAP not implemented yet
+docToString''(DMap ((str, doc) : xs)) acc spaces = ""
+
+docToString'' (DMap[]) acc paces = acc ++ "{}"
+-}
+
+
+-- paskutinis geras, bet tuo paciu ir blogas. iki docToString'''
+
+
+-- docToString :: Document -> String -> String -> String
+
+
+
+
+-- docToString DNull acc spaces = acc ++ "null"
+
+-- docToString (DInteger d) acc spaces = acc ++ show d
+
+-- docToString (DString d) acc spaces = acc ++  d
+
+-- docToString (DList (DMap x : xs)) acc spaces =
+--   docToString' (DList xs)  (docToString (DMap x) (acc ++ "- ") (spaces ++ "  ")) (spaces ++ "")
+
+-- docToString (DList (DList x : xs)) acc spaces =
+--   docToString' (DList xs)  (docToString''' (DList x) (acc ++ "- ") (spaces ++ "  ")) (spaces ++ "") -- neveikia tinkamai
+
+-- docToString (DList (x : xs)) acc spaces =
+--   docToString' (DList xs)  (docToString x (acc ++ spaces ++ "- ") (spaces ++ "")) (spaces ++ "")
+
+-- docToString (DList []) acc spaces =
+--   acc ++ "[]"
+
+-- docToString (DMap((str, DList val) : xs)) acc spaces =
+--   docToString' (DMap xs) (docToString'' (DList val) (acc ++ str ++ ":\n") (spaces ++ "  ")) spaces  -- kai DMape yra Listas
+
+-- docToString (DMap((str, DMap val) : xs)) acc spaces =
+--   docToString' (DMap xs) (docToString''' (DMap val) (acc ++ spaces ++ str ++ ":\n") (spaces ++ "  ")) (spaces ++ "")
+
+-- docToString (DMap((str, val) : xs)) acc spaces =
+--   docToString' (DMap xs) (docToString val (acc ++ str ++ ": ") spaces) (spaces ++ "")
+
+-- docToString (DMap[]) acc spaces = acc ++ "{}"
+
+-- --inner functions for parsing----
+
+-- docToString' :: Document -> String -> String -> String
+
+-- docToString' DNull acc spaces = acc ++ "null"
+
+-- docToString' (DInteger d) acc spaces = acc ++ show d
+
+-- docToString' (DString d) acc spaces = acc ++ d
+
+-- docToString' (DList (DMap x : xs)) acc spaces =
+--   docToString' (DList xs)  (docToString (DMap x) (acc ++ "\n" ++ spaces ++ "- ") (spaces ++ "  ")) (spaces ++ "")
+
+-- docToString' (DList (DList x : xs)) acc spaces =
+--   docToString' (DList xs)  (docToString (DList x) (acc ++ spaces ++ "- ") (spaces ++ "")) (spaces ++ "")
+
+-- docToString' (DList (x : xs)) acc spaces =
+--   docToString' (DList xs) (docToString x (acc ++ "\n" ++ spaces ++ "- ") (spaces ++ "")) (spaces ++ "")
+
+-- docToString' (DList []) acc spaces=
+--   acc ++ ""
+
+-- docToString' (DMap((str, DList val) : xs)) acc spaces =
+--   docToString' (DMap xs) (docToString (DList val) (acc ++ "\n" ++ spaces ++str ++ ":\n") (spaces ++ "")) spaces
+
+-- docToString' (DMap((str, DMap val) : xs)) acc spaces =
+--   docToString' (DMap xs) (docToString (DMap val) (acc ++ "\n" ++ spaces ++str ++ ":\n") (spaces ++ "")) spaces
+
+-- docToString' (DMap((str, val) : xs)) acc spaces=
+--   docToString' (DMap xs) (docToString val (acc ++ "\n" ++ spaces ++ "" ++ str ++ ": ") spaces ) spaces
+
+-- docToString' (DMap[]) acc spaces= acc ++ ""
+
+
+-- docToString'' :: Document -> String -> String -> String
+
+
+
+
+-- docToString'' DNull acc spaces = acc ++ "null"
+
+-- docToString'' (DInteger d) acc spaces = acc ++ show d
+
+-- docToString'' (DString d) acc spaces = acc ++  d
+
+-- docToString'' (DList (DList x : xs)) acc spaces =
+--   docToString' (DList xs)  (docToString (DList x) (acc ++ "- ") (spaces ++ "  ")) (spaces ++ "")
+
+-- docToString'' (DList (x : xs)) acc spaces =
+--   docToString' (DList xs)  (docToString x (acc ++ "- ") (spaces ++ "")) (init(init(spaces)) ++ "")
+
+-- docToString'' (DList []) acc spaces =
+--   acc ++ "[]"
+
+-- docToString'' (DMap((str, DList val) : xs)) acc spaces =
+--   docToString' (DMap xs) (docToString (DList val) (acc ++ str ++ ":\n") spaces) spaces
+
+-- docToString'' (DMap((str, val) : xs)) acc spaces =
+--   docToString' (DMap xs) (docToString val (acc ++ str ++ ": ") (spaces ++ "")) (spaces ++ "")
+
+-- docToString'' (DMap[]) acc spaces = acc ++ "{}"
+
+
+-- docToString''' :: Document -> String -> String -> String
+
+
+
+
+-- docToString''' DNull acc spaces = acc ++ "null"
+
+-- docToString''' (DInteger d) acc spaces = acc ++ show d
+
+-- docToString''' (DString d) acc spaces = acc ++  d
+
+-- docToString''' (DList (DList x : xs)) acc spaces =
+--   docToString' (DList xs)  (docToString (DList x) (acc ++ "- ") (spaces ++ "  ")) (spaces ++ "")
+
+-- docToString''' (DList (x : xs)) acc spaces =
+--   docToString' (DList xs)  (docToString x (acc ++ "- ") (spaces ++ "")) (spaces ++ "")
+
+-- docToString''' (DList []) acc spaces =
+--   acc ++ "[]"
+
+-- docToString''' (DMap((str, DList val) : xs)) acc spaces =
+--   docToString' (DMap xs) (docToString (DList val) (acc ++ spaces ++ str ++ ":\n") spaces) spaces
+
+-- docToString''' (DMap((str, DMap val) : xs)) acc spaces =
+--   docToString' (DMap xs) (docToString''' (DMap val) (acc ++ spaces ++ str ++ ":\n") (spaces ++ "  ")) (spaces ++ "")
+
+-- docToString''' (DMap((str, val) : xs)) acc spaces =
+--   docToString' (DMap xs) (docToString val (acc ++ spaces ++ str ++ ": ") (spaces)) (spaces ++ "")
+
+-- docToString''' (DMap[]) acc spaces = acc ++ spaces ++ "{}"
+
+
+-- docToString'' :: Document -> String -> String -> String
+
+-- docToString'' DNull acc spaces = acc ++ "null"
+
+-- docToString'' (DInteger d) acc spaces = acc ++ show d
+
+-- docToString'' (DString d) acc spaces = acc ++  d
+
+-- docToString'' (DList (x : xs)) acc spaces =
+--   docToString' (DList xs) (docToString'' x (acc ++ "\n- ") (spaces ++ "")) (spaces ++ "")   -- cia kazkas neveikia taip kaip turetu veikti
+
+-- docToString'' (DList []) acc spaces = acc ++"[]" -- not sure ar tarpu turi but tiek kiek dabar. Pasitikrinti kai naudosim encode lib
+
+-- docToString'' (DMap((str, DMap val) : xs)) acc spaces =
+--   docToString' (DMap xs) (docToString'' (DMap val) (acc ++ spaces ++str ++ ":\n") (spaces ++ "  ")) spaces
+
+-- docToString'' (DMap((str, val) : xs)) acc spaces =
+--   docToString' (DMap xs) (docToString'' val (acc ++ spaces ++ str ++ ": ") spaces) spaces
+
+-- docToString'' (DMap[]) acc spaces = acc ++ "{}"  -- not sure ar tarpu turi but tiek kiek dabar. Pasitikrinti kai naudosim encode lib
 
 
 -- docToString :: Document -> String -> String -> String
