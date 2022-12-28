36512e2a78a10958ed69c1760787b892fa87a32c
Andrius <paulauskas.andrius1@gmail.com>
2022-12-29 00:18:37 +0200

    Client to Server connection enabled

--- Client/app4/Main.hs
+++ Client/app4/Main.hs
@@ -5,20 +5,22 @@ import Control.Monad.Trans.Class(lift)
 import Control.Monad.Trans.State.Strict
     ( evalStateT, get, modify, put, StateT )
 import Data.ByteString as B ( empty, ByteString )
-import Data.Either as E (fromRight)
+-- import Data.Either as E (fromRight)
 import qualified Data.List as L
-import Data.Text as T ( concat, drop, pack, unpack, Text )
-import Data.Text.Encoding.Base64 (decodeBase64)
+-- import Data.Text as T ( concat, drop, pack, unpack, Text )
+import Data.Text as T ( concat, pack, unpack, Text )
+-- import Data.Text.Encoding.Base64 (decodeBase64)
 import Data.Text.IO as TIO ( hPutStrLn, putStrLn )
 import Data.List.Split as S ( splitOn )
 import Data.Char (isSpace)
 import Lib1 ( emptyState, mkCheck, render, toggle, State )
 import Lib2 ( renderDocument )
-import Lib3 ( parseDocument, hint, gameStart, Hint, GameStart)
+-- import Lib3 ( parseDocument, hint, gameStart, Hint, GameStart)
+import Lib3 ( parseDocument, gameStart, GameStart)
 import Types(Check, toDocument, fromDocument)
 import Network.Wreq
     ( post, postWith, defaults, header, responseBody )
-import qualified Network.Wreq as Wreq
+-- import qualified Network.Wreq as Wreq
 
 import Control.Lens
 import System.Console.Repline
@@ -39,8 +41,8 @@ type Repl a = HaskelineT (StateT (String, Lib1.State) IO) a
 commandShow :: String
 commandShow = "show"
 
-commandHint :: String
-commandHint = "hint"
+-- commandHint :: String
+-- commandHint = "hint"
 
 commandCheck :: String
 commandCheck = "check"
@@ -55,15 +57,15 @@ cmd c
   | trim c == commandCheck = lift get >>= check . (Lib1.mkCheck . snd) >>= liftIO . Prelude.putStrLn
   | commandToggle `L.isPrefixOf` trim c = do
     case tokens c of
-      [_] -> liftIO $ Prelude.putStrLn $ "Illegal format, \"" ++ commandToggle ++ " expects at leas one argument"
+      [_] -> liftIO $ Prelude.putStrLn $ "Illegal format, \"" ++ commandToggle ++ " expects at least one argument"
       t -> lift $ modify (\(u, s) -> (u, Lib1.toggle s (L.drop 1 t)))
-  | commandHint `L.isPrefixOf` trim c =
-    case tokens c of
-      [_, str] ->
-        case reads str of
-          [(n, "")] -> hints n
-          _ -> liftIO $ Prelude.putStrLn $ "Illegal " ++ commandHint ++ " argument: " ++ str
-      _ -> liftIO $ Prelude.putStrLn $ "Illegal format, \"" ++ commandHint ++ " $number_of_hints\" expected, e.g \"" ++ commandHint ++ " 1\""
+-- | commandHint `L.isPrefixOf` trim c =
+  --   case tokens c of
+  --     [_, str] ->
+  --       case reads str of
+  --         [(n, "")] -> hints n
+  --         _ -> liftIO $ Prelude.putStrLn $ "Illegal " ++ commandHint ++ " argument: " ++ str
+  --     _ -> liftIO $ Prelude.putStrLn $ "Illegal format, \"" ++ commandHint ++ " $number_of_hints\" expected, e.g \"" ++ commandHint ++ " 1\""
 cmd c = liftIO $ Prelude.putStrLn $ "Unknown command: " ++ c
 
 tokens :: String -> [String]
@@ -81,19 +83,20 @@ check c = do
   resp <- liftIO $ postWith opts (url ++ "/check") body
   pure $ cs $ resp ^. responseBody
 
-hints :: Int -> Repl ()
-hints n = do
-  (url, s) <- lift get
-  r <- liftIO $ Wreq.get (url ++ "/hint?limit=" ++ show n)
-  let h = Lib3.parseDocument (cs (r ^. responseBody)) >>= fromDocument
-  case (h :: Either String Lib3.Hint) of
-    Left msg -> liftIO $ fatal $ cs msg
-    Right d -> lift $ put (url, Lib3.hint s d)
+-- hints :: Int -> Repl ()
+-- hints n = do
+--   (url, s) <- lift get
+--   r <- liftIO $ Wreq.get (url ++ "/hint?limit=" ++ show n)
+--   let h = Lib3.parseDocument (cs (r ^. responseBody)) >>= fromDocument
+--   case (h :: Either String Lib3.Hint) of
+--     Left msg -> liftIO $ fatal $ cs msg
+--     Right d -> lift $ put (url, Lib3.hint s d)
 
 -- Tab Completion: return a completion for partial words entered
 completer :: Monad m => WordCompleter m
 completer n = do
-  let names = [commandShow, commandHint, commandCheck, commandToggle]
+  -- let names = [commandShow, commandHint, commandCheck, commandToggle]
+  let names = [commandShow, commandCheck, commandToggle]
   return $ Prelude.filter (L.isPrefixOf n) names
 
 ini :: Repl ()
@@ -127,6 +130,11 @@ main = do
 run :: T.Text -> IO ()
 run token = do
   -- Dear students, it is not against you, it is against silly source code crawlers on the Internet
-  let url = E.fromRight (error "Cannot decode url") $ decodeBase64 $ T.drop 6 "f6675cYmltYXJ1LmhvbWVkaXIuZXU="
-  let fullUrl = T.unpack (T.concat ["http://", url, "/game/", token])
-  evalStateT (evalRepl (const $ pure ">>> ") cmd [] Nothing Nothing (Word completer) ini final) (fullUrl, Lib1.emptyState)
+  -- let url = case decodeBase64 "YmltYXJ1LmhvbWVkaXIuZXU=" of
+  --          Right x -> x
+  --          Left _  -> error "Cannot decode url"
+  -- let url = E.fromRight (error "Cannot decode url") $ decodeBase64 $ T.drop 6 "f6675cYmltYXJ1LmhvbWVkaXIuZXU="
+
+  -- let fullUrl = T.unpack (T.concat ["http://", url, "/game/", token])
+  let fullUrl = T.unpack (T.concat ["http://", "localhost:8080", "/game/", token])
+  evalStateT (evalRepl (const $ pure "==> ") cmd [] Nothing Nothing (Word completer) ini final) (fullUrl, Lib1.emptyState)
--- Server/Main.hs
+++ Server/Main.hs
@@ -3,17 +3,52 @@
 import Network.Wai
 import Network.HTTP.Types
 import Network.Wai.Handler.Warp (run)
-import Data.Aeson
+import Data.Aeson as AE
+import Data.ByteString.Char8 as BS (unpack)
+import Data.Text as T (unpack, intercalate)
+import Data.Yaml as YAML (encode)
+import Data.ByteString.Lazy as BSL (toStrict, fromStrict)
+import Network.Socket (SockAddr (..))
+import System.IO (getLine)
+import Control.Concurrent (forkFinally, killThread, ThreadId)
+import Control.Concurrent.Async (async, cancel, Async)
+import Control.Monad (when)
+import qualified Data.Map as Map
+
+type Token = String
+type Gamestate = String
+
+games :: Map.Map Token Gamestate
+games = Map.empty
 
 app :: Application
-app request respond =
+app request respond = do
+  let token = getToken request
   case requestMethod request of
-    "GET" -> do
-      let responseBody = encode (object ["message" .= ("Hello, World!" :: String)])
-      respond $ responseLBS status200 [(hContentType, "application/json")] responseBody
+    "POST" -> do
+      let responseBody = YAML.encode (object ["message" .= ("Hello, World! " ++ BS.unpack (requestMethod request) ++ token :: String)])
+      respond $ responseLBS status200 [(hContentType, "application/x-yaml")] (BSL.fromStrict responseBody)
     _ -> respond $ responseLBS status405 [(hContentType, "text/plain")] "Method Not Allowed"
 
 main :: IO ()
 main = do
   putStrLn "Listening on port 8080..."
-  run 8080 app
+  serverAsync <- async (run 8080 app)
+  putStrLn "Enter a command ('shutdown' to stop the server):"
+  loop serverAsync
+
+loop :: Async () -> IO ()
+loop serverAsync = do
+  input <- getLine
+  case input of
+    "shutdown" -> cancel serverAsync
+    _ -> do
+      putStrLn "Invalid command"
+      loop serverAsync
+
+
+
+
+getToken :: Request -> Token
+getToken request = T.unpack (T.intercalate "<-->" (pathInfo request))
+  
